a09f2d1cb2e3d579425509eeaec582eb9110acca
mooosakhan <moosakhan3856902@gmail.com>
2025-01-15 20:47:44 +0500

    :sparkles: Updating some files

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,8 +2,8 @@ import './index.css'
 
 const index = () => {
   return (
-    <a href="#contact">
-    <button className="btn-53">
+    <a href="#contact ">
+    <button className="btn-53 pixel-corners"> 
     <div className="original text-xs font-poppins font-extrabold">Contact</div>
 
     {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" height="18px" width="18px"><g stroke-width="0" id="SVGRepo_bgCarrier"></g><g stroke-linejoin="round" stroke-linecap="round" id="SVGRepo_tracerCarrier"></g><g id="SVGRepo_iconCarrier"> <g id="Interface / Download"> <path stroke-linejoin="round" stroke-linecap="round" stroke-width="2" stroke="#f1f1f1" d="M6 21H18M12 3V17M12 17L17 12M12 17L7 12" id="Vector"></path> </g> </g></svg> */}
